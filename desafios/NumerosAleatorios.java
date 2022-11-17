ed4fd91e83522ae6338a1ab9da2ffcbbdf41dae7
jt_sobrinho <64412810+JOSETABOSA47@users.noreply.github.com>
2022-11-17 14:04:10 -0300

    Numeros aleatorios

---
+++ desafios/NumerosAleatorios.java
@@ -0,0 +1,15 @@
+package desafios;
+
+import java.util.Random;
+
+public class NumerosAleatorios {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        System.out.println(random.nextInt(3));
+        // for (int i = 0; i <= 10; i++) {
+        // }
+    }
+}
